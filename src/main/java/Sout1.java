8d22c3cf1502659ac31af162db2df16edd055086
escurse <av1967@naver.com>
2024-10-16 16:23:01 +0900

    JAVA

--- src/main/java/Sout1.java
+++ src/main/java/Sout1.java
@@ -3,5 +3,19 @@ public class Sout1 {
         System.out.println("I am IronMan");
         byte b = 15;
         System.out.println(b);
+        short s = 20;
+        //  int i = 25L; === long i = 25;
+        int i = 25;
+        long l = 30;
+
+        byte b1 = 55;
+        byte b2 = 15;
+        int bs = b1 + b2;
+        // 사칙연산하면 int로 강제 형변환함.
+
+        System.out.println(bs);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
     }
 }
