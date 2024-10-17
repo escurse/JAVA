3540338f41946dfff5ded3b7e7aa91940e0e8644
escurse <av1967@naver.com>
2024-10-17 09:27:22 +0900

    JAVA

--- src/main/java/Sout2.java
+++ src/main/java/Sout2.java
@@ -1,7 +1,8 @@
 public class Sout2 {
-    String message = "";
-
+    String mv;
+    // 멤버 변수
     public static void main(String[] args) {
+                            // 매개 변수
         System.out.println("I am IronMan");
 
         String message = "Hello World";
@@ -14,5 +15,10 @@ public class Sout2 {
         // 기초 타입은 소문자 시작, 약속어
         String s1 = "hello";
         // 참조 타입은 대문자 시작
+
+        String lv;
+        // 지역 변수
+
+
     }
 }
