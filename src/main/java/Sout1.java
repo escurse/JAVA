40c5ad570d1924b2cef687c9b60c3626b96b2a24
escurse <av1967@naver.com>
2024-10-17 09:08:13 +0900

    JAVA

--- src/main/java/Sout1.java
+++ src/main/java/Sout1.java
@@ -1,6 +1,6 @@
 public class Sout1 {
     public static void main(String[] args) {
-//        System.out.println("I am IronMan");
+        System.out.println("I am IronMan");
 //        byte b = 15;
 //        System.out.println(b);
 //        short s = 20;
