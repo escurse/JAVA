5114d7a914f4d8afaad35c29340182f307c2d29e
escurse <av1967@naver.com>
2024-10-17 09:17:41 +0900

    JAVA

--- src/main/java/Sout2.java
+++ src/main/java/Sout2.java
@@ -2,6 +2,7 @@ public class Sout2 {
     public static void main(String[] args) {
         System.out.println("I am IronMan");
 
-
+        String message = "Hello World";
+        System.out.println(message);
     }
 }
