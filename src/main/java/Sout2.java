cd9836bdc11aaf0b448dbb79adce0c87706395e2
escurse <av1967@naver.com>
2024-10-17 09:20:19 +0900

    JAVA

--- src/main/java/Sout2.java
+++ src/main/java/Sout2.java
@@ -4,5 +4,13 @@ public class Sout2 {
 
         String message = "Hello World";
         System.out.println(message);
+
+        byte b = 1;
+        short s = 2;
+        int i = 3;
+        long l = 4;
+        // 기초 타입은 소문자 시작, 약속어
+        String s1 = "hello";
+        // 참조 타입은 대문자 시작
     }
 }
