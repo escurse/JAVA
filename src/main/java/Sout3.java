6c814cacf0a65c847961206190d6b12e25724907
escurse <av1967@naver.com>
2024-10-18 09:46:36 +0900

    JAVA

--- src/main/java/Sout3.java
+++ src/main/java/Sout3.java
@@ -1,5 +1,30 @@
 public class Sout3 {
     public static void main(String[] args) {
+        System.out.println("I am IronMan");
 
+//        String toPrint = "";
+//        toPrint += String.format("신라면\t%10d원\n", 4580);
+//        toPrint += String.format("찰옥수수\t%10d원\n", 12500);
+//        toPrint += String.format("꼬막살\t%10d원\n", 8910);
+//        toPrint += String.format("신라면\t%-10d원\n", 4580);
+//        toPrint += String.format("찰옥수수\t%-10d원\n", 12500);
+//        toPrint += String.format("꼬막살\t%-10d원\n", 8910);
+//        System.out.println(toPrint);
+
+//        String name = "김갑수";
+//        int age = 77;
+//        int price = 157800;
+//        String twoPrint = "제 이름은 " + name + "이고, 나이는 " + age + "살입니다.";
+//        System.out.println(twoPrint);
+//        String toPrint = String.format("제 이름은 %s이고, 나이는 %d살입니다.", name, age);
+//        System.out.println(toPrint);
+//        String threePrint = String.format("총 %,d원 입니다.", price);
+//        System.out.println(threePrint);
+
+//        char [] cs = new char[] {'H', 'e', 'l', 'l', 'o'};
+//        String st = String.copyValueOf(cs);
+//        System.out.println(st);
+////        System.out.println(st[1]);    // X
+//        System.out.println(cs[1]);      // O , e
     }
 }
