5df7098bdc171dd1c56674f8e06656a46f91fcdf
escurse <av1967@naver.com>
2024-10-21 09:53:59 +0900

    JAVA

--- src/main/java/Sout4.java
+++ src/main/java/Sout4.java
@@ -2,14 +2,16 @@ public class Sout4 {
     public static void main(String[] args) {
         Animal animal = new Animal();
         animal.name = "햄스터";
-        animal.move();
+//        animal.move();
 
         Animal cat = new Animal();
         cat.name = "고양이";
-        cat.move();
+//        cat.move();
 
         Animal dog = new Animal();
         dog.name = "강아지";
+        animal.move();
+        cat.move();
         dog.move();
     }
 }
