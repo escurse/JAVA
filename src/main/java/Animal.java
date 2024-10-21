1a7ea2144d5e38f1e1a3181d3f907975d221d38a
escurse <av1967@naver.com>
2024-10-21 09:35:59 +0900

    JAVA

---
+++ src/main/java/Animal.java
@@ -0,0 +1,8 @@
+public class Animal {
+    public static String name;
+
+    public void move() {
+        // Animal의 이동 행동 단위를 구현하기 위한 메서드
+        System.out.println(name + "가(이) 움직였다.");
+    }
+}
--- src/main/java/Sout4.java
+++ src/main/java/Sout4.java
@@ -1,5 +1,15 @@
 public class Sout4 {
     public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.name = "햄스터";
+        animal.move();
 
+        Animal cat = new Animal();
+        cat.name = "고양이";
+        cat.move();
+
+        Animal dog = new Animal();
+        dog.name = "강아지";
+        dog.move();
     }
 }
